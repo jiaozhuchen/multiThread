c3dd60ec5066ac14ee42d64b867a14dea47fbec8
chen.chen <chen.chen1@thoughtworks.com>
2020-06-01 10:12:42 +0800

    [CC] 添加LockSupport类注释

--- src/main/java/util/LockSupportTest.java
+++ src/main/java/util/LockSupportTest.java
@@ -5,6 +5,10 @@ import java.util.concurrent.locks.LockSupport;
 
 import static java.lang.Thread.currentThread;
 
+/**
+ * 测试LockSupport类park unpark的使用
+ *  LockSupport类用于代替Object 的suspend和resume方法（这两个方法因为会造成死锁，已被废弃）
+ */
 public class LockSupportTest {
 
     public static void main(String[] args) throws InterruptedException {

5e0758e62e069079ce24ef3d0537b1988c846ec2
chen.chen <chen.chen1@thoughtworks.com>
2020-06-05 14:11:53 +0800

    [CC] 修改PriorityBlockingQueueTest注释

--- src/main/java/blockqueue/PriorityBlockingQueueTest.java
+++ src/main/java/blockqueue/PriorityBlockingQueueTest.java
@@ -3,7 +3,7 @@ package blockqueue;
 import java.util.concurrent.PriorityBlockingQueue;
 
 /**
- * 测试PriorityBlockingQueue可排序阻塞队列的使用
+ * 测试PriorityBlockingQueue优先级阻塞队列的使用
  *  1. 测试类中如果Person类没有实现Comparable接口，添加Person对象会报错
  *  2. 添加的元素对象会被排序
  *  3.无界队列并不是真的无界，最大元素数 Integer.MAX_VALUE - 8(因为使用数组的原因，底层控制了最大长度)（可以看到比LinkedBlockingQueue最大量还小一些）
